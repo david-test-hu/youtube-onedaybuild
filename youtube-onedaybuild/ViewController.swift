e6bd33ffc9cdfdf3c695fef997fdf53eaec15534
David Gömöri <david@Davids-MacBook-Pro.local>
2020-09-06 11:18:26 +0200

    initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by David Gömöri on 2020. 09. 06..
+//  Copyright © 2020. David Gömöri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
